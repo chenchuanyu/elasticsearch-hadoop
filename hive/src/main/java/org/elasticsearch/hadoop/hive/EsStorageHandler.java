208ae204d222ed39fa8ba482fb51a64387125f23
abel.chan <abel.chan@hudongpai.com>
2018-02-15 11:07:43 +0800

    integration of external interface and add Utils class.

--- hive/src/main/java/org/elasticsearch/hadoop/hive/EsStorageHandler.java
+++ hive/src/main/java/org/elasticsearch/hadoop/hive/EsStorageHandler.java
@@ -33,6 +33,7 @@ import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapred.OutputFormat;
 import org.elasticsearch.hadoop.cfg.HadoopSettingsManager;
 import org.elasticsearch.hadoop.cfg.Settings;
+import org.elasticsearch.hadoop.hive.pushdown.Utils;
 import org.elasticsearch.hadoop.mr.EsOutputFormat;
 import org.elasticsearch.hadoop.mr.HadoopCfgUtils;
 import org.elasticsearch.hadoop.util.Assert;
@@ -119,7 +120,11 @@ public class EsStorageHandler extends DefaultStorageHandler implements HiveStora
     // add pushdown optimizer from hive to es
     @Override
     public DecomposedPredicate decomposePredicate(JobConf jobConf, Deserializer deserializer, ExprNodeDesc exprNodeDesc) {
-        return new EsStoragePredicateHandler().decomposePredicate(jobConf, deserializer, exprNodeDesc);
+        Settings settings = HadoopSettingsManager.loadFrom(jobConf);
+        if (Utils.isPushDown(settings)) {
+            return Utils.getPredicateHander(settings).decomposePredicate(jobConf, deserializer, exprNodeDesc);
+        }
+        return null;
     }
 
     @Override
